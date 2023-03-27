c188bb8f07e33c56c2b00a489b8130c45f510cfc
jcharlpretorius <pretorius.jeancharl@gmail.com>
2023-03-26 22:56:17 -0600

    add customerController functions and db functions

--- backend/models/Customer.js
+++ backend/models/Customer.js
@@ -10,8 +10,8 @@ class Customer {
   }
 
   // Save to DB
-  async save() {
-    let sql = `
+  async create() {
+    const sql = `
       INSERT INTO CUSTOMER(
         FirstName,
         MiddleInitial,
@@ -20,17 +20,28 @@ class Customer {
         Email
       )
       VALUES(
-        '${this.firstName}',
-        '${this.minit}',
-        '${this.lastName}',
-        '${this.address}',
-        '${this.email}'
+        ?,?,?,?,?
       )
     `;
-
+    const payload = [
+      this.firstName,
+      this.minit,
+      this.lastName,
+      this.address,
+      this.email,
+    ];
     // we can use the await syntax because of the pool.promise() in db.js
-    const [newCustomer, _] = await db.execute(sql);
-    return newCustomer;
+    const [newCustomer, _] = await db.execute(sql, payload);
+    const customerId = newCustomer.insertId; // extract primary key
+    // return customer object
+    return {
+      customerId,
+      firstName: this.firstName,
+      minit: this.minit,
+      lastName: this.lastName,
+      address: this.address,
+      email: this.email,
+    };
   }
 
   // Find all customers
@@ -44,9 +55,40 @@ class Customer {
 
   // Find customer by id
   static async findById(customerId) {
-    let sql = `SELECT * FROM CUSTOMER WHERE Customer_ID = ${customerId};`;
-    const [customer, _] = await db.execute(sql);
-    return customer;
+    let sql = `SELECT * FROM CUSTOMER WHERE Customer_ID = ?;`;
+    const [customer, _] = await db.execute(sql, [customerId]);
+    return customer[0];
+  }
+
+  // Update customer
+  static async update(customerId, firstName, minit, lastName, address, email) {
+    let sql = `
+    UPDATE CUSTOMER
+    SET FirstName = ?, MiddleInitial = ?, LastName = ?, Address = ?, Email = ?
+    WHERE Customer_ID = ?
+    `;
+    const payload = [firstName, minit, lastName, address, email, customerId];
+    const [newCustomer, _] = await db.execute(sql, payload);
+    return { firstName, minit, lastName, address, email };
+  }
+
+  // Delete Customer -> I don't think we actually need this
+  static async delete(customerId) {
+    let sql = `DELETE FROM CUSTOMER WHERE Customer_ID = ?;`;
+    await db.execute(sql, [customerId]);
+    return;
+  }
+
+  // Find email
+  static async getEmail(email) {
+    let sql = `
+    SELECT Email
+    FROM CUSTOMER
+    WHERE Email = ?;
+    `;
+    const payload = [email];
+    const [emails, _] = await db.execute(sql, payload);
+    return emails;
   }
 }
 
