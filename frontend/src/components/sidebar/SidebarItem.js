aeada293e2607758352281eca2eb2029c251aa7e
jcharlpretorius <pretorius.jeancharl@gmail.com>
2023-04-04 14:20:51 -0600

    add methods to activate sublinks in the dashboard sidebar

--- frontend/src/components/sidebar/SidebarItem.js
+++ frontend/src/components/sidebar/SidebarItem.js
@@ -2,16 +2,29 @@ import React, { useState } from 'react';
 import { MdKeyboardArrowRight } from 'react-icons/md';
 import { NavLink } from 'react-router-dom';
 
+const activeLink = ({ isActive }) => (isActive ? 'active' : 'link');
+const activeSublink = ({ isActive }) => (isActive ? 'active' : 'link');
+
 const SidebarItem = ({ item, isOpen }) => {
+  const [expandMenu, setExpandMenu] = useState(false);
+
   if (item.children) {
     return (
-      <div className="sidebar-item s-parent open">
+      <div
+        className={
+          expandMenu ? 'sidebar-item s-parent open' : 'sidebar-item s-parent'
+        }
+      >
         <div className="sidebar-title">
           <span>
             {item.icon && <div className="icon">{item.icon}</div>}
             {isOpen && <div>{item.title}</div>}
           </span>
-          <MdKeyboardArrowRight size={25} className="arrow-icon" />
+          <MdKeyboardArrowRight
+            size={25}
+            className="arrow-icon"
+            onClick={() => setExpandMenu(!expandMenu)}
+          />
         </div>
         <div className="sidebar-content">
           {item.children.map((child, index) => {
